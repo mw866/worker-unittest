cf5a2c2738b9cf414eba7aff08dd477917f938b7
Chris Wang <gitlab@chriswang.tech>
2019-04-08 16:33:51 +0800

    Initial commit

---
+++ test/worker-test.js
@@ -0,0 +1,25 @@
+
+before(async function () {
+    Object.assign(global, new (require('@dollarshaveclub/cloudworker'))(require('fs').readFileSync('worker.js', 'utf8')).context);
+});
+
+// You will replace worker.js with the relative path to your worker
+
+const assert = require('assert')
+
+describe('Worker Test', function() {
+    
+    it('returns a body that says The Sum is 4', async function () {
+        let url = new URL('https://worker.example.com')
+        let req = new Request(url)
+        let res = await handleRequest(req)
+        let body = await res.text()
+        assert.equal(body, 'The Sum is 4!')
+    })
+    
+    it('does addition properly', async function() {
+        let res = await addition(1, 1)
+        assert.equal(res, 2)
+    })
+    
+})
